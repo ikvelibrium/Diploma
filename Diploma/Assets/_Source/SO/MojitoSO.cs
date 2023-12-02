7c4ccbe1df907a11e7c5f1f2fd807a6804509eac
ikvelibrium <stalker.alex.perel@mail.ru>
2023-12-02 18:49:46 +0300

    Initial

---
+++ Diploma/Assets/_Source/SO/MojitoSO.cs
@@ -0,0 +1,9 @@
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+public class MojitoSO : ScriptableObject
+{
+    public float WhiteRumNeeded = 40; 
+    public float WhiteRumActual = 0; 
+}
